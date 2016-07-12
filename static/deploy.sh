b9b9a3a57d1769831eb7fbb82be463345d546f5c
Benjamin Tatum <bentatum@me.com>
2016-07-12 15:40:24 -0400

    issue(optimize-speed): losslessly compressed images

--- static/deploy.sh
+++ static/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync . s3://media.lawnandgardenprofessionals.com --profile "leadgrabr" --exclude "deploy.sh" --exclude ".DS_Store" --cache-control "max-age=3600" --expires 2100-01-01T00:00:00Z
+aws s3 sync . s3://media.lawnandgardenprofessionals.com --profile "leadgrabr" --exclude "deploy.sh" --exclude ".DS_Store"
