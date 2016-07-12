37038171d679e11f0723630c2a35d2212d1f5d8f
Benjamin Tatum <bentatum@me.com>
2016-07-12 19:56:51 -0400

    issue(optimize-speed): new max-age cache

--- static/deploy.sh
+++ static/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync . s3://media.lawnandgardenprofessionals.com --profile "leadgrabr" --exclude "deploy.sh" --exclude ".DS_Store"
+aws s3 sync . s3://media.lawnandgardenprofessionals.com --profile "leadgrabr" --exclude "deploy.sh" --exclude ".DS_Store" --cache-control "max-age=604800" --expires 2100-01-01T00:00:00Z
