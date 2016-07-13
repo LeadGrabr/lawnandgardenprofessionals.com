50c534953bfa9780501c97d082b8e06dba1e6c6c
Benjamin Tatum <bentatum@me.com>
2016-07-12 22:24:18 -0400

    issue(optimize-speed): update better-react-flex-slick

--- src/components/LeadForm/index.js
+++ src/components/LeadForm/index.js
@@ -134,7 +134,7 @@ export default class LeadForm extends Component {
               {...inputProps}
             />
           </Box>
-          <Box {...boxProps} pr={isMobile ? 0 : boxSpace}>
+          <Box {...boxProps} pr={column ? 0 : isMobile ? 0 : boxSpace}>
             <JoifulInput
               hideLabel
               name='email'
@@ -150,7 +150,7 @@ export default class LeadForm extends Component {
               {...inputProps}
             />
           </Box>
-          <Box {...boxProps} pr={isMobile ? 0 : boxSpace}>
+          <Box {...boxProps} pr={column ? 0 : isMobile ? 0 : boxSpace}>
             <JoifulInput
               is='select'
               hideLabel
